70936b622b13194233ecf44608eceb095ae0ac16
Pedro Caraballo <pcaraballo@solucionescalamari.com>
2023-06-19 09:46:02 -0500

    Agregando importacion masiva de calificaciones

--- base/utils.py
+++ base/utils.py
@@ -6,13 +6,11 @@ from typing import Any, List
 import pandas as pd
 from teachers.models import Teacher
 from students.models import Student
+from ratings import models as ratings_models
+from subjects import models as subjects_models
 from base import constants
 from dateutil.relativedelta import relativedelta
 from datetime import datetime
-
-
-class UnableToProcessRecordFile(Exception):
-    pass
 
 
 def send_email(subject, body):
@@ -53,7 +51,8 @@ def get_copy_template(object_type: str):
     return generate_excel(file_name=file_name, wb=wb)
 
 
-def generate_dataframe_records(file: Any, object_type: str, file_type: str) -> List[dict]:
+def generate_dataframe_records(file: Any, object_type: str, file_type: str):
+    success = True
     try:
         if file_type in [constants.XLS, constants.XLSX]:
             io_record = pd.ExcelFile(path_or_buffer=file)
@@ -66,53 +65,79 @@ def generate_dataframe_records(file: Any, object_type: str, file_type: str) -> L
 
     except Exception as e:
         print(str(e))
-        raise UnableToProcessRecordFile()
+        success = False
     else:
         if object_type == constants.TEACHERS:
-            dataframe = dataframe.rename(
-                columns={
-                    "DOCUMENTO": "document_number",
-                    "TIPO DOCUMENTO": "document_type",
-                    "NOMBRES": "first_name",
-                    "APELLIDOS": "last_name",
-                    "FECHA NACIMIENTO": "date_of_birth",
-                    "GENERO": "gender",
-                    "TELEFONO": "phone",
-                    "EMAIL": "email",
-                    "DIRECCION": "address"
-                }
-            )
-            dataframe["date_of_birth"] = pd.to_datetime(
-                dataframe["date_of_birth"], errors="coerce"
-            )
-            dataframe["document_type"] = dataframe["document_type"].str.upper()
-            dataframe["gender"] = dataframe["gender"].str.upper()
+            try:
+                dataframe = dataframe.rename(
+                    columns={
+                        "DOCUMENTO": "document_number",
+                        "TIPO DOCUMENTO": "document_type",
+                        "NOMBRES": "first_name",
+                        "APELLIDOS": "last_name",
+                        "FECHA NACIMIENTO": "date_of_birth",
+                        "GENERO": "gender",
+                        "TELEFONO": "phone",
+                        "EMAIL": "email",
+                        "DIRECCION": "address"
+                    }
+                )
+                dataframe["date_of_birth"] = pd.to_datetime(
+                    dataframe["date_of_birth"], errors="coerce"
+                )
+                dataframe["document_type"] = dataframe["document_type"].str.upper()
+                dataframe["gender"] = dataframe["gender"].str.upper()
+            except Exception as e:
+                print(str(e))
+                success = False
 
         elif object_type == constants.STUDENTS:
-            dataframe = dataframe.rename(
-                columns={
-                    "DOCUMENTO": "document_number",
-                    "TIPO DOCUMENTO": "document_type",
-                    "NOMBRES": "first_name",
-                    "APELLIDOS": "last_name",
-                    "FECHA NACIMIENTO": "date_of_birth",
-                    "GENERO": "gender",
-                    "TELEFONO": "phone",
-                }
-            )
-            dataframe["date_of_birth"] = pd.to_datetime(
-                dataframe["date_of_birth"], errors="coerce"
-            )
-            dataframe["document_type"] = dataframe["document_type"].str.upper()
-            dataframe["gender"] = dataframe["gender"].str.upper()
+            try:
+                dataframe = dataframe.rename(
+                    columns={
+                        "DOCUMENTO": "document_number",
+                        "TIPO DOCUMENTO": "document_type",
+                        "NOMBRES": "first_name",
+                        "APELLIDOS": "last_name",
+                        "FECHA NACIMIENTO": "date_of_birth",
+                        "GENERO": "gender",
+                        "TELEFONO": "phone",
+                    }
+                )
+                dataframe["date_of_birth"] = pd.to_datetime(
+                    dataframe["date_of_birth"], errors="coerce"
+                )
+                dataframe["document_type"] = dataframe["document_type"].str.upper()
+                dataframe["gender"] = dataframe["gender"].str.upper()
+            except Exception as e:
+                print(str(e))
+                success = False
+        else:
+            try:
+                dataframe = dataframe.rename(
+                    columns={
+                        "ACTIVIDAD": "activity",
+                        "DOCUMENTO ESTUDIANTE": "student",
+                        "DOCUMENTO PROFESOR": "teacher",
+                        "CODIGO CURSO": "subject",
+                        "CALIFICACION": "rating_number",
+                        "OBSERVACION": "observations",
+                    }
+                )
+            except Exception as e:
+                print(str(e))
+                success = False
 
-        return dataframe.to_dict(orient="records")
+        if success:
+            return dataframe.to_dict(orient="records")
+        return success
 
 
 def save_records(records: List[dict], object_type: str) -> dict:
+
     success_amount = 0
     errors_amount = 0
-    object_name = ""
+
     if object_type == constants.TEACHERS:
         object_name = "Profesor(es)"
         for item in records:
@@ -161,6 +186,55 @@ def save_records(records: List[dict], object_type: str) -> dict:
                     date_of_birth=item["date_of_birth"],
                     gender=item["gender"],
                     phone=phone
+                )
+                success_amount += 1
+            except Exception as e:
+                print(str(e))
+                errors_amount += 1
+                continue
+    else:
+        object_name = "Calificación(es)"
+        for item in records:
+
+            # check if active student exist
+            try:
+                student = Student.objects.get(document_number=item["student"], status=1)
+            except Student.DoesNotExist:
+                errors_amount += 1
+                continue
+
+            # check if active teacher exist
+            try:
+                teacher = Teacher.objects.get(document_number=item["teacher"], status=1)
+            except Teacher.DoesNotExist:
+                errors_amount += 1
+                continue
+
+            # check if active subject exist
+            try:
+                subject = subjects_models.Subject.objects.get(code=item["subject"], status=1)
+            except subjects_models.Subject.DoesNotExist:
+                errors_amount += 1
+                continue
+
+            # check if teacher is subject's teacher
+            if teacher.id != subject.teacher_id:
+                errors_amount += 1
+                continue
+
+            # check if student is in subject's students
+            if student not in subject.students.all():
+                errors_amount += 1
+                continue
+
+            try:
+                ratings_models.Rating.objects.create(
+                    activity=item["activity"],
+                    student=student,
+                    teacher=teacher,
+                    subject=subject,
+                    rating_number=item["rating_number"],
+                    observations=item["observations"],
                 )
                 success_amount += 1
             except Exception as e:
